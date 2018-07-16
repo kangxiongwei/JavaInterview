6694bf4227deb857c305c81288342ab5bade3965
亢雄伟 <kangxiongwei@meituan.com>
2018-07-16 11:56:57 +0800

    循环迭代List删除元素问题

--- JavaInterview/src/main/java/com/kxw/conllections/ArrayListTest.java
+++ JavaInterview/src/main/java/com/kxw/conllections/ArrayListTest.java
@@ -7,12 +7,47 @@ import java.util.*;
  */
 public class ArrayListTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        test02();
+
+    }
+
+    private static void test02() {
+        List<Integer> list = Arrays.asList(5, 7, 8, 6, 1, 2, 3, 4);
+
+        /**
+         * 会抛异常，因为会走迭代器，modCount和expectModCount不相等异常
+         */
+        /*for (Integer i: list) {
+            System.out.println(i);
+            list.remove(i);
+        }*/
+
+        /**
+         * 不会抛异常，但是由于size在变化，i也在变化，导致取得结果不正确
+         */
+        /*for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+            list.remove(list.get(i));
+        }*/
+
+        /**
+         * 正确姿势，因为iterator内部expectModCount会被修改为modCount，删除元素后游标会回退1格
+         */
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Integer value = iterator.next();
+            System.out.println(value);
+            iterator.remove();
+        }
+    }
+
+    private static void test01() {
         List<String> list = new ArrayList<String>();
         PriorityQueue<String> queue = new PriorityQueue<String>();
-        Map<String,String> map2 = new HashMap<String, String>();
-        LinkedHashMap<String,String> map = new LinkedHashMap<String, String>();
-        map.put("kang","xiongwei");
+        Map<String, String> map2 = new HashMap<String, String>();
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        map.put("kang", "xiongwei");
         queue.add("kang");
         queue.add("xiong");
         queue.add("wei");
